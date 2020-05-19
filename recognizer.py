ea270f3faac85a8bed2c95c10046d4b34ec75866
matan-mac <matanby94@gmail.com>
2020-05-19 10:23:01 +0300

    Change confidence into a new variable

--- recognizer.py
+++ recognizer.py
@@ -86,13 +86,13 @@ while True:
         # Check if confidence is less them 100 ==> "0" is perfect match 
         if (confidence < 70):
             id = names[id]
-            confidence = "  {0}%".format(round(100 - confidence))
+            confidencePct = "  {0}%".format(round(100 - confidence))
         else:
             id = "unknown"
-            confidence = "  {0}%".format(round(100 - confidence))
+            confidencePct = "  {0}%".format(round(100 - confidence))
 
         cv2.putText(img, str(id), (x+5,y-5), font, 1, (255,255,255), 2)
-        cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)
+        cv2.putText(img, str(confidencePct), (x+5,y+h-5), font, 1, (255,255,0), 1)
 
         if (confidence < 70):
             imageName = "detection/User." + str(id) + '.' + randomString(10) + ".jpg"
